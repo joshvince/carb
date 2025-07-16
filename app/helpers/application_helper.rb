4f9e16354183e9d317e4c1b3a3451cd4bec6b798
Josh Vince <joshmvince@gmail.com>
2025-07-16 20:35:18 +0100

    add different views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,10 +13,6 @@ module ApplicationHelper
     end.sort_by { |carb| -carb[:corrigan_score] }
   end
 
-  def calculate_percentage(value, total)
-    total > 0 ? (value.to_f / total * 100).round(1) : 0
-  end
-
   def calculate_score_percentage(score, max_score)
     max_score > 0 ? (score.to_f / max_score * 100).round(1) : 0
   end
@@ -30,13 +26,20 @@ module ApplicationHelper
     end
   end
 
-  def first_place_percentage_label(percentage)
-    "#{percentage}% ranked 1st"
-  end
+  def weighted_havoc_ratio_view_data(local_assigns)
+    emojis = {
+      bread: '🍞',
+      noodles: '🍜',
+      pasta: '🍝',
+      potato: '🥔',
+      rice: '🍚'
+    }
 
-  def position_distribution_label(carb_total, position, total_answers)
-    count = carb_total.public_send("total_#{position}")
-    percentage = calculate_percentage(count, total_answers)
-    "#{ranking_label(position)} #{percentage}%"
+    total_votes = local_assigns[:total_answers]
+
+    local_assigns.slice(:bread, :noodles, :pasta, :potato, :rice).map do |key, value|
+      havoc_ratio = total_votes > 0 ? (value.total_1.to_f / total_votes * 100).round(1) : 0
+      { name: key.to_s.humanize, emoji: emojis[key], havoc_ratio: havoc_ratio }
+    end.sort_by { |carb| -carb[:havoc_ratio] }
   end
 end
